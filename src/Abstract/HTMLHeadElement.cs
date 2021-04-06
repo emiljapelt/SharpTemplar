386e1b05cd2eeb7e9135eaeac0207c5b206cebce
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2021-04-06 21:53:39 +0200

    Reorder

--- src/Abstract/HTMLHeadElement.cs
+++ src/Abstract/HTMLHeadElement.cs
@@ -30,18 +30,18 @@ namespace Elements
             sb.Append($"</{tagType}>");
         }
 
-        public HTMLHeadElement AddStyle(string path)
-        {
-            var style = new Style(path);
-            Contains.Add(style);
-            return style;
-        }
-
         public HTMLHeadElement WithAttribute(string key, string value)
         {
             if (Attributes.ContainsKey(key)) Attributes[key] = value;
             else Attributes.Add(key, value);
             return this;
+        }
+
+        public HTMLHeadElement AddStyle(string path)
+        {
+            var style = new Style(path);
+            Contains.Add(style);
+            return style;
         }
 
         public HTMLHeadElement AddMeta()
