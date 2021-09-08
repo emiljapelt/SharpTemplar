b8e73325c2fd32605ed94b3c35562427b23c2198
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2021-09-08 14:43:04 +0200

    Implement AddDefaults in Head

--- src/Head/Head/Head.cs
+++ src/Head/Head/Head.cs
@@ -9,5 +9,14 @@ namespace SharpTemplar
         {
             Contains.Add(new Title(title));
         }
+
+        public HTMLHeadElement AddDefaults()
+        {
+            AddMeta().WithAttribute("charset","UTF-8");
+            AddMeta().WithAttributes(("name","viewport"),("content","width=device-width, initial-scale=1.0"));
+
+            ResetThisElement();
+            return this;
+        }
     }
 }
--- testapi/Controllers/TestController.cs
+++ testapi/Controllers/TestController.cs
@@ -23,9 +23,8 @@ namespace testapi.Controllers
         {
             var doc = new FreeFormDocument("test");
             
-            doc.Head.AddMeta().WithAttribute("charset","utf-8");
+            doc.Head.AddDefaults();
             doc.Head.AddScript("./scripts/test.js");
-            doc.Head.AddMeta().WithAttributes(("name","viewport"),("size","large"));
 
             doc.Body.AddButton("button","click me!").OnEvent(click, "test1");
             doc.Body.AddButton("button","click me too!").OnEvent(click, "test3", "hi", 2);
