4efbdd2f4715383f04dd7884896e4be43084e344
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-08-13 10:05:47 +0200

    Implement monadic loop and conditional

--- src/Monadic/Bundles/Base/Base.cs
+++ src/Monadic/Bundles/Base/Base.cs
@@ -9,6 +9,8 @@ public static partial class Base
         return new MarkupMonad(new HTMLtag("html"), new HashSet<string>()); 
     }
 
+    public static Functor nothing = (monad) => monad;
+
     public static MMonad text(this MMonad m, string input) { return applyFunctor(Text(input), m); }
     public static Functor Text(string input) {
         return (monad) => {
@@ -19,15 +21,33 @@ public static partial class Base
         };
     }
 
-    public static MMonad enter(this MMonad m) { return applyFunctor(Enter, m); }
-    public static Functor Enter = (monad) => {
+    public static MMonad Enter(this MMonad m) { return applyFunctor(enter, m); }
+    public static Functor enter = (monad) => {
         if (monad.newest is null) return FailWith("No newest added element to Enter!");
         else return new MarkupMonad(monad.newest, monad.ids);
     };
 
-    public static MMonad exit(this MMonad m) { return applyFunctor(Exit, m); }
-    public static Functor Exit = (monad) => {
+    public static MMonad Exit(this MMonad m) { return applyFunctor(exit, m); }
+    public static Functor exit = (monad) => {
         if (monad.pointer.parent is null) return FailWith("No parent to Exit to!");
         else return new MarkupMonad(monad.pointer.parent, monad.ids);
+    };
+
+    public static MMonad If(this MMonad m, bool b, Functor e, Functor o) { return applyFunctor(Cond(b, e, o), m); }
+    public static MMonad If(this MMonad m, bool b, Functor e) { return applyFunctor(Cond(b, e, nothing), m); }
+    public static Func<bool, Functor, Functor, Functor> Cond = (condition, either, or) => {
+        if (condition) return either;
+        else return or;
+    };
+
+    public static MMonad Range(this MMonad m, int count, Func<int, Functor> f) { return applyFunctor(Loop(0, count, f), m); }
+    public static MMonad Range(this MMonad m, int start, int count, Func<int, Functor> f) { return applyFunctor(Loop(start, count, f), m); }
+    public static MMonad Range(this MMonad m, int count, Functor f) { return applyFunctor(Loop(0, count, (i) => f), m); }
+    public static MMonad Range(this MMonad m, int start, int count, Functor f) { return applyFunctor(Loop(start, count, (i) => f), m); }
+    public static Func<int, int, Func<int, Functor>, Functor> Loop = (start, count, f) => (monad) => {
+        for(int i = start; i < start + count; i++) {
+            f(i)(monad);
+        }
+        return monad;
     };
 }
--- tester/Program.cs
+++ tester/Program.cs
@@ -9,30 +9,29 @@ public class Program
     {
         Functor box = (monad) => {
             return monad
-                .div().@class("box").enter()
-                    .p().text("I am a box").exit();
+                .div().@class("box").Enter()
+                    .p().text("I am a box").Exit();
         };
 
         Functor numbox(int num) {
             return (monad) => {
                 return monad
-                    .div().@class("box").enter()
-                        .p().text($"I am box {num}").exit();
+                    .div().@class("box").Enter()
+                        .p().text($"I am box {num}").Exit();
             };
         }
 
         var s = Markup()
             .head()
-            .body().enter()
-                .audio().enter()
-                    .source("audio/mpeg", "horse.mp3").exit()
+            .body().Enter()
+                .audio().Enter()
+                    .source("audio/mpeg", "horse.mp3").Exit()
                 .img("meme.jpg", "just imagine a meme")
                 .h(2).@id("1").text("MEMEs")
-                .div().@id("2").@class("outerbox").@class("meme").enter()
-                    ._(box).exit()
+                .div().@id("2").@class("outerbox").@class("meme").Enter()
+                    ._(box).Exit()
                 ._(box)
-                ._(numbox(1))
-                ._(numbox(2))
+                .Range(4, (i) => numbox(i))
         .Build();
 
         Console.WriteLine(s);
