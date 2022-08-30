6a161a8ec53c166bb05610fa2f52463f5c62210d
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-08-30 15:09:17 +0200

    Reference the same context arrays for events

--- src/Monadic/Bundles/Events/Events.cs
+++ src/Monadic/Bundles/Events/Events.cs
@@ -43,28 +43,33 @@ public static partial class Events
         else return (monad) => new MarkupFailure("Unknown EventInfo type!");
     }
 
+    private static readonly string[] anyContext = new string[]{};
+    private static readonly string[] bodyOnly = new string[]{"body"};
+    private static readonly string[] usualExclusives = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"};
+
+
     // Window events
-    public static EventInfo afterprint = new InclusiveEventInfo() { eventName = "afterprint", contexts = new string[]{"body"} };
-    public static EventInfo beforeprint = new InclusiveEventInfo() { eventName = "beforeprint", contexts = new string[]{"body"} };
-    public static EventInfo beforeupload = new InclusiveEventInfo() { eventName = "beforeupload", contexts = new string[]{"body"} };
-    public static EventInfo error = new InclusiveEventInfo() { eventName = "error", contexts = new string[]{"body"} };
-    public static EventInfo hashchange = new InclusiveEventInfo() { eventName = "hashchange", contexts = new string[]{"body"} };
-    public static EventInfo load = new InclusiveEventInfo() { eventName = "load", contexts = new string[]{"body"} };
-    public static EventInfo message = new InclusiveEventInfo() { eventName = "message", contexts = new string[]{"body"} };
-    public static EventInfo offline = new InclusiveEventInfo() { eventName = "offline", contexts = new string[]{"body"} };
-    public static EventInfo online = new InclusiveEventInfo() { eventName = "online", contexts = new string[]{"body"} };
-    public static EventInfo pagehide = new InclusiveEventInfo() { eventName = "pagehide", contexts = new string[]{"body"} };
-    public static EventInfo pageshow = new InclusiveEventInfo() { eventName = "pageshow", contexts = new string[]{"body"} };
-    public static EventInfo popstate = new InclusiveEventInfo() { eventName = "popstate", contexts = new string[]{"body"} };
-    public static EventInfo resize = new InclusiveEventInfo() { eventName = "resize", contexts = new string[]{"body"} };
-    public static EventInfo storage = new InclusiveEventInfo() { eventName = "storage", contexts = new string[]{"body"} };
-    public static EventInfo unload = new InclusiveEventInfo() { eventName = "unload", contexts = new string[]{"body"} };
+    public static EventInfo afterprint = new InclusiveEventInfo() { eventName = "afterprint", contexts = bodyOnly };
+    public static EventInfo beforeprint = new InclusiveEventInfo() { eventName = "beforeprint", contexts = bodyOnly };
+    public static EventInfo beforeupload = new InclusiveEventInfo() { eventName = "beforeupload", contexts = bodyOnly };
+    public static EventInfo error = new InclusiveEventInfo() { eventName = "error", contexts = bodyOnly };
+    public static EventInfo hashchange = new InclusiveEventInfo() { eventName = "hashchange", contexts = bodyOnly };
+    public static EventInfo load = new InclusiveEventInfo() { eventName = "load", contexts = bodyOnly };
+    public static EventInfo message = new InclusiveEventInfo() { eventName = "message", contexts = bodyOnly };
+    public static EventInfo offline = new InclusiveEventInfo() { eventName = "offline", contexts = bodyOnly };
+    public static EventInfo online = new InclusiveEventInfo() { eventName = "online", contexts = bodyOnly };
+    public static EventInfo pagehide = new InclusiveEventInfo() { eventName = "pagehide", contexts = bodyOnly };
+    public static EventInfo pageshow = new InclusiveEventInfo() { eventName = "pageshow", contexts = bodyOnly };
+    public static EventInfo popstate = new InclusiveEventInfo() { eventName = "popstate", contexts = bodyOnly };
+    public static EventInfo resize = new InclusiveEventInfo() { eventName = "resize", contexts = bodyOnly };
+    public static EventInfo storage = new InclusiveEventInfo() { eventName = "storage", contexts = bodyOnly };
+    public static EventInfo unload = new InclusiveEventInfo() { eventName = "unload", contexts = bodyOnly };
 
     // Form events
-    public static EventInfo blur = new ExclusiveEventInfo() { eventName = "blur", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
+    public static EventInfo blur = new ExclusiveEventInfo() { eventName = "blur", contexts = usualExclusives };
     public static EventInfo change = new InclusiveEventInfo() { eventName = "change", contexts = new string[]{"input", "select", "textarea"} };
     public static EventInfo contextmenu = new InclusiveEventInfo() { eventName = "contextmenu", contexts = new string[]{} };
-    public static EventInfo focus = new ExclusiveEventInfo() { eventName = "focus", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
+    public static EventInfo focus = new ExclusiveEventInfo() { eventName = "focus", contexts = usualExclusives };
     public static EventInfo input = new InclusiveEventInfo() { eventName = "input", contexts = new string[]{"input", "textarea"} };
     public static EventInfo invalid = new InclusiveEventInfo() { eventName = "invalid", contexts = new string[]{"input"} };
     public static EventInfo reset = new InclusiveEventInfo() { eventName = "reset", contexts = new string[]{"form"} };
@@ -72,34 +77,34 @@ public static partial class Events
     public static EventInfo submit = new InclusiveEventInfo() { eventName = "submit", contexts = new string[]{"form"} };
     
     // Keyboard events
-    public static EventInfo keydown = new ExclusiveEventInfo() { eventName = "keydown", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo keypress = new ExclusiveEventInfo() { eventName = "keypress", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo keyup = new ExclusiveEventInfo() { eventName = "keyup", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
+    public static EventInfo keydown = new ExclusiveEventInfo() { eventName = "keydown", contexts = usualExclusives };
+    public static EventInfo keypress = new ExclusiveEventInfo() { eventName = "keypress", contexts = usualExclusives };
+    public static EventInfo keyup = new ExclusiveEventInfo() { eventName = "keyup", contexts = usualExclusives };
 
     // Mouse events
-    public static EventInfo click = new ExclusiveEventInfo() { eventName = "click", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo dblclick = new ExclusiveEventInfo() { eventName = "dblclick", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo mousedown = new ExclusiveEventInfo() { eventName = "mousedown", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo mousemove = new ExclusiveEventInfo() { eventName = "mousemove", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo mouseout = new ExclusiveEventInfo() { eventName = "mouseout", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo mouseover = new ExclusiveEventInfo() { eventName = "mouseover", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo mouseup = new ExclusiveEventInfo() { eventName = "mouseup", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
-    public static EventInfo wheel = new ExclusiveEventInfo() { eventName = "wheel", contexts = new string[]{"base", "bdo", "br", "head", "html", "iframe", "meta", "param", "script", "style", "title"} };
+    public static EventInfo click = new ExclusiveEventInfo() { eventName = "click", contexts = usualExclusives };
+    public static EventInfo dblclick = new ExclusiveEventInfo() { eventName = "dblclick", contexts = usualExclusives };
+    public static EventInfo mousedown = new ExclusiveEventInfo() { eventName = "mousedown", contexts = usualExclusives };
+    public static EventInfo mousemove = new ExclusiveEventInfo() { eventName = "mousemove", contexts = usualExclusives };
+    public static EventInfo mouseout = new ExclusiveEventInfo() { eventName = "mouseout", contexts = usualExclusives };
+    public static EventInfo mouseover = new ExclusiveEventInfo() { eventName = "mouseover", contexts = usualExclusives };
+    public static EventInfo mouseup = new ExclusiveEventInfo() { eventName = "mouseup", contexts = usualExclusives };
+    public static EventInfo wheel = new ExclusiveEventInfo() { eventName = "wheel", contexts = usualExclusives };
 
     // Drag events
-    public static EventInfo drag = new InclusiveEventInfo() { eventName = "drag", contexts = new string[]{} };
-    public static EventInfo dragend = new InclusiveEventInfo() { eventName = "dragend", contexts = new string[]{} };
-    public static EventInfo dragenter = new InclusiveEventInfo() { eventName = "dragenter", contexts = new string[]{} };
-    public static EventInfo dragleave = new InclusiveEventInfo() { eventName = "dragleave", contexts = new string[]{} };
-    public static EventInfo dragover = new InclusiveEventInfo() { eventName = "dragover", contexts = new string[]{} };
-    public static EventInfo dragstart = new InclusiveEventInfo() { eventName = "dragstart", contexts = new string[]{} };
-    public static EventInfo drop = new InclusiveEventInfo() { eventName = "drop", contexts = new string[]{} };
-    public static EventInfo scroll = new InclusiveEventInfo() { eventName = "scroll", contexts = new string[]{} };
+    public static EventInfo drag = new InclusiveEventInfo() { eventName = "drag", contexts = anyContext };
+    public static EventInfo dragend = new InclusiveEventInfo() { eventName = "dragend", contexts = anyContext };
+    public static EventInfo dragenter = new InclusiveEventInfo() { eventName = "dragenter", contexts = anyContext };
+    public static EventInfo dragleave = new InclusiveEventInfo() { eventName = "dragleave", contexts = anyContext };
+    public static EventInfo dragover = new InclusiveEventInfo() { eventName = "dragover", contexts = anyContext };
+    public static EventInfo dragstart = new InclusiveEventInfo() { eventName = "dragstart", contexts = anyContext };
+    public static EventInfo drop = new InclusiveEventInfo() { eventName = "drop", contexts = anyContext };
+    public static EventInfo scroll = new InclusiveEventInfo() { eventName = "scroll", contexts = anyContext };
 
     // Clipboard events
-    public static EventInfo copy = new InclusiveEventInfo() { eventName = "copy", contexts = new string[]{} };
-    public static EventInfo cut = new InclusiveEventInfo() { eventName = "cut", contexts = new string[]{} };
-    public static EventInfo paste = new InclusiveEventInfo() { eventName = "paste", contexts = new string[]{} };
+    public static EventInfo copy = new InclusiveEventInfo() { eventName = "copy", contexts = anyContext };
+    public static EventInfo cut = new InclusiveEventInfo() { eventName = "cut", contexts = anyContext };
+    public static EventInfo paste = new InclusiveEventInfo() { eventName = "paste", contexts = anyContext };
 
 
 }
